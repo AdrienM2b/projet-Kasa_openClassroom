5f4f802bd87ad8e7d2e7af0b09a131072970fb2e
AdrienM2b <adrien.marchetti.6@gmail.com>
2023-12-07 15:09:59 +0100

    ajout des pages routes

---
+++ my-app/src/routes/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <header>
+      <div>
+        <img src='' alt='' />
+      </div>
+      <div></div>
+    </header>
+  );
+}
